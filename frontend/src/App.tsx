6fc5846d7b0cbf6b5adcfd8aa7638b2cc02e380a
parsely <parsely_1231@icloud.com>
2020-04-29 17:25:09 +0900

    re first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>AtCoder Code Evaluate</div>
+    <div>
+      <div>AtCoder Code Evaluate</div>
+      <p>やっと環境構築が終わった</p>
+    </div>
   );
 };
 
