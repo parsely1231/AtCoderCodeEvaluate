6f0256ad09816f5162ec817956f1da6188bc7745
parsely <parsely_1231@icloud.com>
2020-06-18 20:34:01 +0900

    add mimetypes to settings.py

--- backend/django_api/src/config/settings.py
+++ backend/django_api/src/config/settings.py
@@ -1,6 +1,7 @@
 import os
 import environ
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
